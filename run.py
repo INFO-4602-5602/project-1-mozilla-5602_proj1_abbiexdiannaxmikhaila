9bcbe7246c44374613f22f11415d1df8dfc8f857
Mikhaila Friske <frisk028@umn.edu>
2019-11-04 09:17:32 -0700

    add some icons to use as buttons. update the details page to have legends

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import http.server
 import socketserver
+import zipfile
 
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 if __name__ == "__main__":
+  print("Unzipping data....")
+  with zipfile.ZipFile("data.zip", 'r') as zip_ref:
+    zip_ref.extractall()
+    
   print("Running....")
   with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
